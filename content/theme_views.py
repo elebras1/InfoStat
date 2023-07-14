a6ef5ef0c7f38cf40cd6b2d80d87c16ceee60ac9
Erwan <erwan.lebras27@gmail.com>
2023-07-14 14:31:17 +0200

    theme page, update on article and infographie

---
+++ content/theme_views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render, get_object_or_404
+from django.http import Http404
+from .models import Theme, Infographie, Article
+
+
+def theme(request, id):
+    theme = get_object_or_404(Theme, pk=id)
+    infographies = Infographie.objects.filter(theme=theme).order_by("-pub_date")[:6]
+    articles = Article.objects.filter(theme=theme).order_by("-pub_date")[:6]
+    theme.compteur += 1
+    theme.save()
+
+    return render(
+        request,
+        "theme.html/",
+        {"theme": theme, "infographies": infographies, "articles": articles},
+    )
--- content/urls.py
+++ content/urls.py
@@ -1,8 +1,9 @@
-from . import views, secteur_views, infographie_views, article_views
+from . import views, secteur_views, infographie_views, article_views, theme_views
 from django.urls import include, path
 
 urlpatterns = [
     path("secteur", secteur_views.secteur, name="secteur"),
     path("infographie/<int:id>/", infographie_views.infographie, name="infographie"),
     path("article/<int:id>/", article_views.article, name="article"),
+    path("theme/<int:id>/", theme_views.theme, name="theme"),
 ]
