491ce3dc97ea8cc8eb97a1b9d99ca715ca104630
Erwan <erwan.lebras27@gmail.com>
2023-07-21 16:40:42 +0200

    update

--- user/forms/registration_form.py
+++ user/forms/registration_form.py
@@ -2,11 +2,53 @@ from django import forms
 
 
 class RegistrationForm(forms.Form):
-    username = forms.CharField(label="Pseudo*", max_length=100)
-    email = forms.CharField(label="Email*", max_length=100)
-    last_name = forms.CharField(label="Prénom", max_length=100, required=False)
-    first_name = forms.CharField(label="Nom", max_length=100, required=False)
-    password = forms.CharField(label="Mot de passe*", max_length=128)
+    username = forms.CharField(
+        max_length=100,
+        widget=forms.TextInput(
+            attrs={
+                "class": "block w-full rounded border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+            }
+        ),
+    )
+    email = forms.EmailField(
+        max_length=100,
+        widget=forms.EmailInput(
+            attrs={
+                "class": "block w-full rounded border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+            }
+        ),
+    )
+    last_name = forms.CharField(
+        max_length=100,
+        required=False,
+        widget=forms.TextInput(
+            attrs={
+                "class": "block w-full rounded border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+            }
+        ),
+    )
+    first_name = forms.CharField(
+        max_length=100,
+        required=False,
+        widget=forms.TextInput(
+            attrs={
+                "class": "block w-full rounded border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+            }
+        ),
+    )
+    password = forms.CharField(
+        max_length=128,
+        widget=forms.PasswordInput(
+            attrs={
+                "class": "block w-full rounded border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+            }
+        ),
+    )
     password_confirmation = forms.CharField(
-        label="Confirmation du mot de passe*", max_length=100
+        max_length=100,
+        widget=forms.PasswordInput(
+            attrs={
+                "class": "block w-full rounded border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+            }
+        ),
     )
