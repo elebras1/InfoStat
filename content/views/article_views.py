f9f683e8485c9ca7056dfab242bcc94c35b7ab02
Erwan <erwan.lebras27@gmail.com>
2023-07-25 11:27:49 +0200

    add favori option

--- content/views/article_views.py
+++ content/views/article_views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import Http404
-from ..models import Article, Infographie
+from ..models import Article, Infographie, Article_favori
 
 
 def article(request, id):
@@ -13,6 +13,37 @@ def article(request, id):
     article.compteur += 1
     article.save()
 
+    user = request.user
+
+    if user.is_authenticated:
+        favori = Article_favori.objects.filter(
+            user=user,
+            article=article,
+        )
+
+        etat_favori = favori.exists()
+
+    else:
+        etat_favori = False
+
+    if request.method == "POST" and user.is_authenticated:
+        if not favori:
+            new_favori = Article_favori.objects.create(
+                user=user,
+                article=article,
+            )
+            etat_favori = True
+            print("insert")
+        else:
+            Article_favori.objects.filter(
+                user=user,
+                article=article,
+            ).delete()
+            etat_favori = False
+            print("delete")
+
+    print(etat_favori)
+
     return render(
         request,
         "article.html",
@@ -20,5 +51,6 @@ def article(request, id):
             "article": article,
             "article_selection": article_selection,
             "infographie_selection": infographie_selection,
+            "etat_favori": etat_favori,
         },
     )
--- content/views/infographie_views.py
+++ content/views/infographie_views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import Http404
-from ..models import Infographie, Article
+from ..models import Infographie, Article, Infographie_favori
 
 
 def infographie(request, id):
@@ -13,6 +13,33 @@ def infographie(request, id):
     infographie.compteur += 1
     infographie.save()
 
+    user = request.user
+
+    if user.is_authenticated:
+        favori = Infographie_favori.objects.filter(
+            user=user,
+            infographie=infographie,
+        )
+
+        etat_favori = favori.exists()
+
+    else:
+        etat_favori = False
+
+    if request.method == "POST" and user.is_authenticated:
+        if not favori:
+            new_favori = Infographie_favori.objects.create(
+                user_id=user.id,
+                infographie_id=infographie.id,
+            )
+            etat_favori = True
+        else:
+            Infographie_favori.objects.filter(
+                user=user,
+                infographie=infographie,
+            ).delete()
+            etat_favori = False
+
     return render(
         request,
         "infographie.html",
@@ -20,5 +47,6 @@ def infographie(request, id):
             "infographie": infographie,
             "infographie_selection": infographie_selection,
             "article_selection": article_selection,
+            "etat_favori": etat_favori,
         },
     )
